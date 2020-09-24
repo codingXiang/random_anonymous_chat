f7e4ac74a02b43f9cf56418166c0cc13daa38542
xiang <dryadf66449@gmail.com>
2020-09-24 21:11:07 +0800

    init

--- go.mod
+++ go.mod
@@ -3,7 +3,20 @@ module anonymous_chat
 go 1.14
 
 require (
+	github.com/codingXiang/configer v1.0.2-0.20200513072245-ec8070de9a16
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-redis/redis/v8 v8.1.3
+	github.com/google/uuid v1.1.2
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/magiconair/properties v1.8.4 // indirect
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/spf13/afero v1.4.0 // indirect
+	github.com/spf13/viper v1.7.1
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/sys v0.0.0-20200923182605-d9f96fdee20d // indirect
+	gopkg.in/ini.v1 v1.61.0 // indirect
 	gopkg.in/olahol/melody.v1 v1.0.0-20170518105555-d52139073376
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"github.com/codingXiang/configer"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
+	"github.com/spf13/viper"
 	"gopkg.in/olahol/melody.v1"
+	"log"
 	"net/http"
 )
 
@@ -12,6 +19,11 @@ type Message struct {
 	Name    string `json:"name"`
 	Content string `json:"content"`
 }
+var (
+	KEY  = ""
+	WAIT = ""
+)
+
 
 func NewMessage(event, name, content string) *Message {
 	return &Message{
@@ -26,7 +38,38 @@ func (m *Message) GetByteMessage() []byte {
 	return result
 }
 
+var redisClient *redis.Client
+var conf *viper.Viper
+
+func init() {
+	config := configer.NewConfigerCore("yaml", "config", "./config", ".")
+	config.SetAutomaticEnv("")
+
+	if c, err := config.ReadConfig(nil); err == nil {
+		conf = c
+		KEY = conf.GetString("application.chat.id")
+		WAIT = conf.GetString("application.chat.wait")
+	} else {
+		panic(err)
+	}
+
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", conf.GetString("redis.url"), conf.GetInt("redis.port")),
+		Password: conf.GetString("redis.password"), // no password set
+		DB:       conf.GetInt("redis.db"),        // use default DB
+	})
+
+	pong, err := redisClient.Ping(context.Background()).Result()
+	if err == nil {
+		log.Println("redis 回應成功，", pong)
+	} else {
+		log.Fatal("redis 無法連線，錯誤為", err)
+	}
+}
+
 func main() {
+
+
 	r := gin.Default()
 	r.LoadHTMLGlob("template/html/*")
 	r.Static("/assets", "./template/assets")
@@ -40,18 +83,66 @@ func main() {
 	})
 
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		m.Broadcast(msg)
+		id := GetSessionID(s)
+		chatTo, _ := redisClient.Get(context.TODO(), id).Result()
+		m.BroadcastFilter(msg, func(session *melody.Session) bool {
+			compareID, _ := session.Get(KEY)
+			return compareID == chatTo || compareID == id
+		})
 	})
 
 	m.HandleConnect(func(session *melody.Session) {
-		id := session.Request.URL.Query().Get("id")
-		m.Broadcast(NewMessage("other", id, "加入聊天室").GetByteMessage())
+		id := InitSession(session)
+		if key, err := GetWaitFirstKey(); err == nil && key != "" {
+			CreateChat(id, key)
+			msg := NewMessage("other", "對方已經", "加入聊天室").GetByteMessage()
+			m.BroadcastFilter(msg, func(session *melody.Session) bool {
+				compareID, _ := session.Get(KEY)
+				return compareID == id || compareID == key
+			})
+		} else {
+			AddToWaitList(id)
+		}
 	})
 
 	m.HandleClose(func(session *melody.Session, i int, s string) error {
-		id := session.Request.URL.Query().Get("id")
-		m.Broadcast(NewMessage("other", id, "離開聊天室").GetByteMessage())
-		return nil
+		id := GetSessionID(session)
+		chatTo, _ := redisClient.Get(context.TODO(), id).Result()
+		msg := NewMessage("other", "對方已經", "離開聊天室").GetByteMessage()
+		RemoveChat(id, chatTo)
+		return m.BroadcastFilter(msg, func(session *melody.Session) bool {
+			compareID, _ := session.Get(KEY)
+			return compareID == chatTo
+		})
 	})
-	r.Run(":5000")
+	r.Run(fmt.Sprintf(":%d", conf.GetInt("application.port")))
+}
+
+func AddToWaitList(id string) error {
+	return redisClient.LPush(context.Background(), WAIT, id).Err()
+}
+
+func GetWaitFirstKey() (string, error) {
+	return redisClient.LPop(context.Background(), WAIT).Result()
+}
+
+func CreateChat(id1, id2 string) {
+	redisClient.Set(context.Background(), id1, id2, 0)
+	redisClient.Set(context.Background(), id2, id1, 0)
+}
+
+func RemoveChat(id1, id2 string) {
+	redisClient.Del(context.Background(), id1, id2)
+}
+func GetSessionID(s *melody.Session) string {
+	if id, isExist := s.Get(KEY); isExist {
+		return id.(string)
+	}
+	return InitSession(s)
+}
+
+func InitSession(s *melody.Session) string {
+	id := uuid.New().String()
+	s.Set(KEY, id)
+	return id
 }
